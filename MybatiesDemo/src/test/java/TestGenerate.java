2508aef60f040c658ef29faca0ff15015a9b3502
Andrew <anluanlu123@163.com>
2017-05-25 15:12:50 +0800

    commit

--- MybatiesDemo/src/test/java/TestGenerate.java
+++ MybatiesDemo/src/test/java/TestGenerate.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
+import com.anlu.model.student;
 import java.io.IOException;
 import java.io.Reader;
 
@@ -19,7 +20,7 @@ public class TestGenerate {
 
     @Before
     public void prepare() throws IOException {
-        String resource = "mybatis-config.xml";
+        String resource = "generate-mybatis-config.xml";
         Reader reader = org.apache.ibatis.io.Resources.getResourceAsReader(resource);
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(reader);
@@ -34,19 +35,18 @@ public class TestGenerate {
 //        stu.setSchool("华明中学");
 //        stu.setStuNum("1001");
 //
-//       int result = sqlSession.insert("com.anlu.mapper.studentMapper.insert",stu);
+//       int result = sqlSession.insert("studentMapper.insert",stu);
 //        System.out.println("结果"+result);
 //    }
 //
-//    @Test
-//    public void findone(){
-//
-//        Long stuId = new Long(1);
-//
-//       student stu = sqlSession.selectOne("com.anlu.mapper.studentMapper.selectByPrimaryKey",stuId);
-//       System.out.println(stu.getSchool());
-//
-//    }
+    @Test
+    public void findone(){
+        Long stuId = new Long(1);
+
+       student stu = sqlSession.selectOne("com.anlu.mapper.studentMapper.selectByPrimaryKey",stuId);
+       System.out.println(stu.getSchool());
+
+    }
 
     @After
     public void after()throws IOException{
